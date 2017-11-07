25b02827743a7bc4d591a64dc2b058fd9fd4f916
Casey <casey@Caseys-MacBook-Pro.local>
2017-11-07 15:56:41 -0500

    Refactored code, tests work

--- features/login.feature
+++ features/login.feature
@@ -4,8 +4,8 @@ I want users to be authenticated before using the product
 So that only registered users have access
 
 Scenario: User logs in
-Given I navigate to "apps.mypurecloud.com"
-When I type "myuser" into "#email"
-And I type "mypassword" into "#password"
-And I click "#btn-login"
-Then "available" is contained in the class of the element with selector "entity-mini-card-container"
+Given I navigate to "https://apps.mypurecloud.com"
+When I type "agent@cobrowsedev.inin" into "#email"
+And I type "CobrowseTest1234!" into "#password"
+And I click ".btn-login"
+Then "available" is contained in the class of the element with selector ".entity-mini-card-container"
--- features/step_definitions/login_steps.js
+++ features/step_definitions/login_steps.js
@@ -1,25 +1,31 @@
-const { cucumber } = require('cucumber');
+const { defineSupportCode } = require('cucumber');
 const assert = require('assert');
 
-cucumber(function({ Given, When, Then, And }) {
+defineSupportCode(function({ Given, When, Then, And, setDefaultTimeout }) {
+
+	setDefaultTimeout(60000);
 
 	Given(/^I navigate to \"([^\"]*)\"$/, function(text) {
-		return this.page.goto(`${string}`);
+		return this.page.goto(text);
 	});
 
-	When(/^I navigate to \"([^\"]*)\"$/, async function(text, selector) {
+	When(/^I type \"([^\"]*)\" into \"([^\"]*)\"$/, async function(text, selector) {
 		await this.page.waitForSelector(selector, 5000);
 		await this.page.type(selector, text);
 	});
 
-	And(/^I click \"([^\"]*)\"$/, async function(selector) {
+	When(/^I click \"([^\"]*)\"$/, async function(selector) {
 		await this.page.waitForSelector(selector, 5000);
 		await this.page.click(selector);
 	});
 
 	Then(/^\"([^\"]*)\" is contained in the class of the element with selector \"([^\"]*)\"$/, async function(text, selector) {
-		await this.page.waitForSelector(selector, 5000);
-		const elementText = await this.page.$eval(selector, el => el.value);
-		assert(elementText === text);
+		await this.page.waitForSelector(`${selector}.${text}`, 10000);
+
+		const classAttribute = await this.page.evaluate((selector) => {
+			return document.querySelector(selector).getAttribute('class')
+		}, selector);
+
+		assert(classAttribute.includes(text));
 	});
 });
